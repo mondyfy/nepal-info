fecba97f8cf41ab9a6bdca13a14a0f13f556bbe5
developer-sandip <khaptadisandip@gmail.com>
2020-01-27 16:50:07 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Info Nepal
+
+Jan. 27, 2020
+<br />
+Info Nepal is a tiny npm package that provides the lists of the districts of Nepal in a bulk as well as according to Province.
---
+++ index.js
@@ -0,0 +1,5 @@
+const allDistricts = require('./lib/districts');
+const districtsOfZone = require('./lib/districtsZone');
+const districtsOfProvince = require('./lib/districtsProvince');
+
+module.exports = { allDistricts, districtsOfZone, districtsOfProvince };
