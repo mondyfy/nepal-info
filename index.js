b817d954829c72285657ffeeae0110a73c960efc
Anwesh <abperni@gmail.com>
2020-01-28 13:41:16 +0545

    Added district name for municipalities

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const allDistricts = require('./lib/districts');
 const districtsOfZone = require('./lib/districtsZone');
 const districtsOfProvince = require('./lib/districtsProvince');
+const municipality = require('./lib/municipality');
 
-module.exports = { allDistricts, districtsOfZone, districtsOfProvince };
+module.exports = { allDistricts, districtsOfZone, districtsOfProvince, municipality };
