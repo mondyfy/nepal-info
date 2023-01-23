a94ec1a7bb3fe217f105f881e9e64390b82d0ba6
Shivaji Chalise <respond2shivaji@gmail.com>
2023-01-23 09:04:29 +0545

    Typo fix and added new data into index.js

--- index.js
+++ index.js
@@ -3,4 +3,19 @@ const districtsOfZone = require('./lib/districtsZone');
 const districtsOfProvince = require('./lib/districtsProvince');
 const localBodies = require('./lib/localBodies');
 
-module.exports = { allDistricts, districtsOfZone, districtsOfProvince, localBodies };
+// In Devanagari
+const allDistrictsInDevanagari = require('./lib/districtsInDevanagari');
+const districtsOfProvinceInDevanagari = require('./lib/districtsProvinceInDevanagari');
+const localBodiesInDevanagari = require('./lib/localBodiesInDevanagari');
+
+module.exports = {
+  allDistricts,
+  districtsOfZone,
+  districtsOfProvince,
+  localBodies,
+
+  // in devanagari 
+  allDistrictsInDevanagari,
+  districtsOfProvinceInDevanagari,
+  localBodiesInDevanagari
+};
